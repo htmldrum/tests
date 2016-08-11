e26a6953939985697066ffc7f6d5861ddb6faad9
htmldrum <htmldrum@tutanota.com>
2016-08-11 10:32:27 +1000

    Init.

---
+++ ruby/each_test.rb
@@ -0,0 +1,9 @@
+N = 1_000_000
+nums = N.times.map{ rand(N) }
+
+require 'benchmark'
+Benchmark.bmbm do |x|
+  x.report('inject'){nums.inject({}){ |h,n| h[n]=n; h }}
+  x.report('tap/each'){{}.tap{ |h| nums.each{ |n| h[n]=n }}}
+  x.report('ea_wi_obj'){nums.each_with_object({}){ |n,h| h[n]=n}}
+end
